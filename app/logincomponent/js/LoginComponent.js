a95b7b9885285bd9c3f595b6564b221d1297d582
wasimakram-mulla <wasim3ace@gmail.com>
2017-07-12 16:09:41 +0530

    added controller

--- app/logincomponent/js/LoginComponent.js
+++ app/logincomponent/js/LoginComponent.js
@@ -4,6 +4,11 @@ angular.module('heroApp').component('heroDetail', {
   templateUrl: 'logincomponent/logincomponent.html',
   bindings: {
     hero: '='
-  }
+  },
+  controller: LoginController
 });
+
+function LoginController(){
+	alert("Test")
+}
 })(window.angular);
