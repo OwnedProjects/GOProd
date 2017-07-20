37b91d3536eab9e435db6e23db1b297035a5afdb
Mulla <Wasimakram.Mulla@roundpointmortgage.com>
2017-07-20 17:33:33 -0400

    added modal

--- app/components/client/clientlistcomponent/js/clientlistcomponent.js
+++ app/components/client/clientlistcomponent/js/clientlistcomponent.js
@@ -9,8 +9,52 @@ angular.module('GreenApp').config(function($stateProvider){
     templateUrl: 'components/client/clientlistcomponent/clientlist.html',
     controller: ClientlistComponent,
     controllerAs: 'clientlistCtrl',
-});
+})
+.controller('EditModalInstanceCtrl', EditModalInstanceCtrl);
 
-function ClientlistComponent($state){
-    
-}
+ClientlistComponent.$inject = ["$uibModal", "$timeout"];
+
+function ClientlistComponent($uibModal, $timeout){
+    var vm = this;
+	vm.items = [1,2,3,4,5];
+	vm.testData = "Hello Message";
+	vm.openEditDialog = openEditDialog;
+	
+	function openEditDialog(size){
+		var modalInstance = $uibModal.open({
+			animation: true,
+			templateUrl: 'myModalContent.html',
+			size: size,
+			controller: 'EditModalInstanceCtrl',
+			controllerAs: 'modctrl',
+			resolve: {
+				items: function () {
+				return vm.items;
+			}
+      }
+		});
+		
+		
+		modalInstance.result.then(function (selectedItem) {
+			console.log("dismissed", selectedItem);
+		}, function () {
+			console.log('Modal dismissed at: ' + new Date());
+		});
+	};	
+};
+
+function EditModalInstanceCtrl($uibModalInstance, items) {
+	var modctrl = this;
+	modctrl.items = items;
+	modctrl.selected = {
+		item: modctrl.items[0]
+	};
+
+	modctrl.ok = function () {
+		$uibModalInstance.close(modctrl.selected.item);
+	};
+
+	modctrl.cancel = function () {
+		$uibModalInstance.dismiss('cancel');
+	};
+};
