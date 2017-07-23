53fefca5affd60847112949447ad8d340a18b4d4
Avees Panchbhai <Avees.Panchbhai@splus.local>
2017-07-23 21:41:34 +0530

    db exporte

--- app/db/gto.sql
+++ app/db/gto.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 22, 2017 at 02:36 PM
+-- Generation Time: Jul 23, 2017 at 06:10 PM
 -- Server version: 10.1.13-MariaDB
 -- PHP Version: 5.5.37
 
@@ -38,6 +38,35 @@ CREATE TABLE `client_master` (
   `client_status` varchar(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `supplier_master`
+--
+
+CREATE TABLE `supplier_master` (
+  `supplier_id` int(10) NOT NULL,
+  `supplier_name` varchar(50) DEFAULT NULL,
+  `vat` varchar(20) DEFAULT NULL,
+  `product` varchar(50) DEFAULT NULL,
+  `contact_person` varchar(50) DEFAULT NULL,
+  `city` varchar(50) DEFAULT NULL,
+  `contactno` varchar(20) DEFAULT NULL,
+  `address` varchar(100) DEFAULT NULL,
+  `supplier_status` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `supplier_master`
+--
+
+INSERT INTO `supplier_master` (`supplier_id`, `supplier_name`, `vat`, `product`, `contact_person`, `city`, `contactno`, `address`, `supplier_status`) VALUES
+(1, 'NAIMUDDIN', '12312', 'Slaughter House Waste', 'wasim', 'pune', '+919028775269', 'H.No.794/1, 1st floor sadik building, adarsh nagar,\npune', 'active'),
+(2, 'sufiyan n panchbhai', '12312', 'Raw Organic Manure', 'wsaim', 'Pune', '+919028775269', 'teset', 'active'),
+(3, 'avees', '12312', 'slaughter', 'wasim', 'pune', '9821211212', 'Katraj', 'active'),
+(4, 'avees', '12312', 'slaughter', 'wasim', 'pune', '9821211212', 'Katraj', 'active'),
+(5, 'avees', '12312', 'slaughter', 'wasim', 'pune', '9821211212', 'Katraj', 'active');
+
 --
 -- Indexes for dumped tables
 --
@@ -49,6 +78,12 @@ ALTER TABLE `client_master`
   ADD PRIMARY KEY (`client_id`);
 
 --
+-- Indexes for table `supplier_master`
+--
+ALTER TABLE `supplier_master`
+  ADD PRIMARY KEY (`supplier_id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -56,7 +91,12 @@ ALTER TABLE `client_master`
 -- AUTO_INCREMENT for table `client_master`
 --
 ALTER TABLE `client_master`
-  MODIFY `client_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `client_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `supplier_master`
+--
+ALTER TABLE `supplier_master`
+  MODIFY `supplier_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
