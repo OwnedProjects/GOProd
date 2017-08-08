e9a1eb1444b193689f7acd0d98e6acd16e814113
wasimakram-mulla <wasim3ace@gmail.com>
2017-08-08 19:46:44 +0530

    db updated

--- app/db/gto.sql
+++ app/db/gto.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Aug 05, 2017 at 08:53 PM
+-- Generation Time: Aug 08, 2017 at 04:15 PM
 -- Server version: 5.5.25a
 -- PHP Version: 5.4.4
 
@@ -38,6 +38,68 @@ CREATE TABLE IF NOT EXISTS `client_master` (
   `client_status` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`client_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `production_batch_master`
+--
+
+CREATE TABLE IF NOT EXISTS `production_batch_master` (
+  `batch_id` int(10) NOT NULL AUTO_INCREMENT,
+  `batch_no` varchar(10) DEFAULT NULL,
+  `bags` varchar(10) DEFAULT NULL,
+  `echomeal` varchar(10) DEFAULT NULL,
+  `prod_date` varchar(15) DEFAULT NULL,
+  `batch_status` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`batch_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+
+--
+-- Dumping data for table `production_batch_master`
+--
+
+INSERT INTO `production_batch_master` (`batch_id`, `batch_no`, `bags`, `echomeal`, `prod_date`, `batch_status`) VALUES
+(1, 'B12', '20', '10', '1502130600000', 'open'),
+(2, 'B786', '50', '10', '1502303400000', 'open'),
+(3, 'B22', '25', '10', '1502303400000', 'open'),
+(4, 'BN1', '123', '8', '1502217000000', 'open');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `production_batch_register`
+--
+
+CREATE TABLE IF NOT EXISTS `production_batch_register` (
+  `batch_reg_id` int(11) NOT NULL AUTO_INCREMENT,
+  `prod_id` varchar(10) DEFAULT NULL,
+  `quantity` varchar(20) DEFAULT NULL,
+  `batch_id` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`batch_reg_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=17 ;
+
+--
+-- Dumping data for table `production_batch_register`
+--
+
+INSERT INTO `production_batch_register` (`batch_reg_id`, `prod_id`, `quantity`, `batch_id`) VALUES
+(1, '1', '3', '1'),
+(2, '2', '1', '1'),
+(3, '3', '2', '1'),
+(4, '4', '4', '1'),
+(5, '1', '2', '2'),
+(6, '2', '2', '2'),
+(7, '3', '2', '2'),
+(8, '4', '4', '2'),
+(9, '1', '4', '3'),
+(10, '2', '2', '3'),
+(11, '3', '2', '3'),
+(12, '4', '2', '3'),
+(13, '1', '2', '4'),
+(14, '2', '2', '4'),
+(15, '3', '2', '4'),
+(16, '4', '2', '4');
 
 -- --------------------------------------------------------
 
@@ -89,8 +151,8 @@ CREATE TABLE IF NOT EXISTS `purchase_master` (
 --
 
 INSERT INTO `purchase_master` (`purchase_id`, `supplier_id`, `purchase_date`, `bill_date`, `bill_no`, `lorry_no`, `weight`, `rate`, `lorryfreight`, `bags`, `total_amount`) VALUES
-(4, '8', '1501871400000', '1502303400000', 'B1212', 'MH 12 LL 2803', '20', '500', '0', NULL, NULL),
-(5, '9', '1501698600000', '1504117800000', '02', 'MH 12 AA 2320', '50', '20', '0', NULL, NULL),
+(4, '8', '1501871400000', '1502303400000', 'B1212', 'MH 12 LL 2803', '20', '500', '0', NULL, '10000'),
+(5, '9', '1501698600000', '1504117800000', '02', 'MH 12 AA 2320', '50', '20', '0', NULL, '1000'),
 (6, '12', '1501957800000', '1502303400000', 'B22', 'MH 12 LL 2803', NULL, NULL, NULL, '200', '2000'),
 (7, '11', '1501957800000', '1502303400000', 'B212', 'MH 12 LL 2803', NULL, NULL, NULL, '300', '3200'),
 (8, '11', '1502044200000', '1502908200000', 'Test', 'MH 12 AA 2320', NULL, NULL, NULL, '120', '1200'),
