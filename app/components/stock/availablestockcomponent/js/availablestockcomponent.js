d4b8f843f4e5ce11e205bf643fe15320a8756680
wasimakram-mulla <wasim3ace@gmail.com>
2017-08-09 14:06:49 +0530

    stock files renamed

--- app/components/stock/availablestockcomponent/js/availablestockcomponent.js
+++ app/components/stock/availablestockcomponent/js/availablestockcomponent.js
@@ -2,18 +2,39 @@ angular.module('GreenApp').config(function($stateProvider){
     $stateProvider
         .state('home.availablestock', {
             url: '/availablestock',
-            component: 'availablestockComponent'
+            component: 'availablestockcomponent'
         })
 })
-.component('availablestockComponent', {
+.component('availablestockcomponent', {
     templateUrl: 'components/stock/availablestockcomponent/availablestock.html',
     controller: AvailablestockController,
-    controllerAs: 'availablestockCtrl'
+    controllerAs: 'availstockCtrl'
 });
 
 AvailablestockController.$inject = ["StockService", "LogService"];
 
-function AvailablestockController(){
+function AvailablestockController(StockService, LogService){
     var vm = this;
+    vm.product = null;
     vm.stockProduct = null;
+    vm.bags = null;
+    vm.ecomeal = null;
+    vm.init = init;
+
+    function init(){
+        StockService.getDetails()
+        .then(function(response){
+            vm.stockProduct = response;
+            vm.ecomeal = [vm.stockProduct[vm.stockProduct.length-1]];
+            vm.bags = [vm.stockProduct[vm.stockProduct.length-2]];
+            console.log(vm.ecomeal)
+            console.log(vm.bags);
+        })
+        .catch(function(error){
+            console.log(error);
+        })
+    }
+
+    vm.init();
+
 }
